2e9a938e6e1d080bf6a2a6784bba277b9a8de5da
mdashikqbl <mdashikqbl@gmail.com>
2022-05-29 15:11:47 +0600

    create a delete api

--- index.js
+++ index.js
@@ -52,6 +52,13 @@ async function run() {
             const result = await orderCollection.insertOne(order);
             res.send(result);
         })
+
+        app.delete('/order/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await orderCollection.deleteOne(query);
+            res.send(result);
+        })
     }
     finally {
 
